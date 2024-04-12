60234a16fe405f7451a06c86ed43cfa8fceda0a8
notmaiku <mike1davis@rocketmail.com>
2024-04-12 02:26:38 -0400

    initial commit
    takes in answer as json and returns the answer
    also has cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { cors } from '@elysiajs/cors'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => 'Default works!')
+  .post('purple', ({ body: { answer } }) => `Answered ${answer}`, {
+    body: t.Object({
+      answer: t.String()
+    })
+  })
+  .use(cors({ origin: '*' }))
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
